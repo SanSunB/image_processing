10627d9d09624811d8a1008c3aeb7e684fc60b28
sun bitan <sanbitan3@gmail.com>
2019-06-20 14:28:14 +0300

    Basic testing

--- knots_package/Pixel.py
+++ knots_package/Pixel.py
@@ -1,10 +1,6 @@
 class Pixel:
     """ Represent the color value of a pixel in the image. """
     # Methods
-    def __init__(self):
-        # color [r,g,b] list
-        self.color = [0, 0, 0]
-
-    def __init__(self, color):
+    def __init__(self, color=[0, 0, 0]):
         # color [r,g,b] list
         self.color = color
